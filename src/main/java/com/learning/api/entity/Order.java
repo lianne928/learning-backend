f6d211f9fd19d52845dc75dccf1bf3bf6ac54dd1
rogerhou2000 <rogerhou2000@gmail.com>
2026-03-09 23:59:36 +0800

    feat: booking轉 Order 實體類別

--- src/main/java/com/learning/api/entity/Order.java
+++ src/main/java/com/learning/api/entity/Order.java
@@ -5,24 +5,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "bookings")
+@Table(name = "orders")
 @Getter
 @Setter
-public class Booking {
+public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-<<<<<<< HEAD
     @Column(name = "user_id", nullable = false)
     private Long userId;
-=======
-    // 先 true
-    @Column(name = "order_id", nullable = true)
-    private Long orderId;
->>>>>>> 7354f396dc9c7a39fe91fe05968dd303e0bd21c9
 
-    @Column(name = "course_id", nullable = false)
+    @Column(name = "booking_id", nullable = false)
     private Long courseId;
 
     @Column(name = "unit_price", nullable = false)

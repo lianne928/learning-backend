30152c1032b02050598bfca36ae9a75cb2578ee0
林伯罕 <u1062248@gmail.com>
2026-03-11 11:17:37 +0800

    feat: 新增 MemberController 和 MemberRepo，實現用戶註冊與登入功能

---
+++ src/main/java/com/learning/api/controller/MemberController.java
@@ -0,0 +1,39 @@
+package com.learning.api.controller;
+
+import com.learning.api.entity.User;
+import com.learning.api.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/auth")
+public class MemberController {
+
+    @Autowired
+    private MemberService memberService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user){
+
+        if (!memberService.register(user)) {
+            return ResponseEntity.status(400).body(Map.of("msg", "註冊失敗"));
+        }
+
+        return ResponseEntity.ok(Map.of("msg", "歡迎"));
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User member){
+
+        if (!memberService.login(member)){
+            return ResponseEntity.status(401).body(Map.of("msg", "帳號或密碼錯誤"));
+        }
+
+        return ResponseEntity.ok(Map.of("msg", "歡迎"));
+    }
+}
---
+++ src/main/java/com/learning/api/repo/MemberRepo.java
@@ -0,0 +1,12 @@
+package com.learning.api.repo;
+
+
+import com.learning.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepo extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
